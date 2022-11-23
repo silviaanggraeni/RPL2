692fa0ae76f29c4bb8380ea45a3cffc0cc3270b9
silviaanggraeni <silviaanggraeni0909@gmail.com>
2022-11-23 20:39:45 +0700

    pertemuan 5

--- src/com/toko/bunga/service/TokoBungaService.java
+++ src/com/toko/bunga/service/TokoBungaService.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.toko.bunga.dao;
+package com.toko.bunga.service;
 
 import com.toko.bunga.model.TokoBunga;
 import java.util.List;
@@ -12,7 +12,7 @@ import java.util.List;
  *
  * @author salis
  */
-public interface TokoBungaDao {
+public interface TokoBungaService {
     public void save(TokoBunga tokobunga);
     public void update(TokoBunga tokobunga);
     public void delete(TokoBunga tokobunga);
---
+++ src/com/toko/bunga/service/TokoBungaServiceImpl.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.toko.bunga.service;
+
+import com.toko.bunga.dao.TokoBungaDao;
+import com.toko.bunga.model.TokoBunga;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ *
+ * @author salis
+ */
+@Service("TokoBungaService")
+@Transactional(readOnly = true)
+public class TokoBungaServiceImpl implements TokoBungaService {
+    @Autowired 
+    private TokoBungaDao tokoBungaDao;
+    
+    @Override
+    public void save(TokoBunga tokobunga) {
+        tokoBungaDao.save(tokobunga);
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void update(TokoBunga tokobunga) {
+        tokoBungaDao.update(tokobunga);
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete(TokoBunga tokobunga) {
+        tokoBungaDao.delete(tokobunga);
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<TokoBunga> getList() {
+        return tokoBungaDao.getList();
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
